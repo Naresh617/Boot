b128e3f40f289a0d5ea9a9d989427cac1a761ea4
Naresh <nareshreddynagiri@gmail.com>
2021-02-12 18:06:44 +0530

    log4j also added this project

--- SpringRestTemplate/src/main/java/com/naresh/controller/TotoController.java
+++ SpringRestTemplate/src/main/java/com/naresh/controller/TotoController.java
@@ -2,6 +2,8 @@ package com.naresh.controller;
 
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,11 +16,17 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class TotoController {
 
+	
+	private static final Logger logger = LogManager.getLogger(TotoController.class);
+	
 	@Autowired
 	RestTemplate restTemplate;
-
+  
+	
+	
 	@GetMapping("/todslist")
 	public String todoList() {
+		logger.info("Hello from Log4j 2 - num ");
 		HttpHeaders headers =new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity <String> entity = new HttpEntity<>(headers);
