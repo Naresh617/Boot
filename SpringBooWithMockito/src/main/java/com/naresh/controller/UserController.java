613bdaa327f0580584354d5591d759fc8f1e5135
Naresh <nareshreddynagiri@gmail.com>
2021-02-17 17:12:04 +0530

    update added to usercontrol

--- SpringBooWithMockito/src/main/java/com/naresh/controller/UserController.java
+++ SpringBooWithMockito/src/main/java/com/naresh/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.naresh.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,16 @@ public class UserController {
 	@DeleteMapping(value = "/remove/{id}")
 	public void removeUser(@PathVariable int id) {
 		userServices.deleteUser(id);
+	}
+	
+	@GetMapping(value="/user/{id}")
+	public Optional<User> getUserById(@PathVariable int id) {
+		return  userServices.getUserById(id);
+	}
+	
+	@PutMapping("/saveUser")
+	public User UpdateUser(@RequestBody User user) {
+		return userServices.addUser(user);
 	}
 
 }
