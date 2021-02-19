d7322deb27d33a45246f5c1d88e999791a096e1c
Naresh <nareshreddynagiri@gmail.com>
2021-02-19 10:46:57 +0530

    transfter

---
+++ SpringBatch/target/classes/schema-all.sql
@@ -0,0 +1,6 @@
+CREATE TABLE People (
+    Personid INT NOT NULL AUTO_INCREMENT,
+    LastName VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(255),
+    PRIMARY KEY (Personid)
+);
---
+++ SpringBootJdbcTemplate/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, name, email) values(1,'Admin','admin@gmail.com');
+insert into users(id, name, email) values(2,'Laxmi','Laxmi@gmail.com');
+insert into users(id, name, email) values(3,'Krishna','Krishna@gmail.com');
